5b88a5a0b7702cd8e01a9fcbcc4f4455fb464dbe
Giovanni Azevedo <giovanniadev@gmail.com>
2024-10-23 21:17:17 -0300

    fix routing issue for deploy

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,19 @@
 // src/services/routes/Routes.tsx
 import React from 'react'
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { HashRouter, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import routesConfig from './routesConfig'
 
 const AppRoutes: React.FC = () => (
-  <Router>
+  <HashRouter>
+      <Router>
     <Routes>
       {routesConfig.map(route => (
         <Route key={route.title} path={route.path} element={route.element} />
       ))}
     </Routes>
   </Router>
+  </HashRouter>
+
 )
 
 export default AppRoutes
