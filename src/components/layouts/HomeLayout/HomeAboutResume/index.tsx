1232f79dcd2ba24f28e8a836ea98f459d918a84b
Matheus Diógenes <matheusdiogenesdev@gmail.com>
2024-10-31 17:30:19 -0300

    enviando

--- src/components/layouts/HomeLayout/HomeAboutResume/index.tsx
+++ src/components/layouts/HomeLayout/HomeAboutResume/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Box } from "@chakra-ui/react";
 import MovLayout from "../../MovLayout";
 import "./style.css"; // Importando o CSS
@@ -6,57 +6,77 @@ import "./style.css"; // Importando o CSS
 type SmallAboutProps = {};
 
 export const SmallAbout: React.FC<SmallAboutProps> = () => {
-  return (
-    <>
-      <MovLayout maxWidthContainer="100vw">
-        <Box className="small-about-container">
-          <Box className="small-about-header">
-            <h1 className="small-about-title">Quem somos</h1>
-            <hr className="small-about-divider" />
-          </Box>
-          <Box className="small-about-description">
-            <h1 className="small-about-subtitle">
-              {" "}
-              Bem-vindo à MOV Produtora!
-            </h1>
-            <p className="small-about-text">
-              Aqui transformamos suas ideias em imagens incríveis! Com mais de
-              1000 vídeos produzidos, e centenas de clientes satisfeitos, somos
-              especialistas em capturar momentos inesquecíveis e criar conteúdos
-              visuais de alta qualidade. Nossos serviços incluem:
-            </p>
-            <ul className="custom-checklist">
-              <li className="small-about-text">
-                Fotografia Corporativa e Institucional: Capturamos a essência da
-                sua marca, destacando o que a torna autêntica e memorável.
-              </li>
-              <li className="small-about-text">
-                Cobertura de Eventos: De aniversários a eventos corporativos,
-                registramos cada detalhe com cuidado e criatividade para
-                eternizar esses momentos especiais.
-              </li>
-              <li className="small-about-text">
-                Filmagens Aéreas: Oferecemos uma visão diferenciada e
-                impressionante, realçando a beleza e a grandiosidade de cada
-                projeto.
-              </li>
-              <li className="small-about-text">
-                Animações e Motion Graphics: Transformamos ideias em animações
-                envolventes, que informam e encantam o público.
-              </li>
-            </ul>
+  const [isVisible, setIsVisible] = useState(false);
+  const sectionRef = useRef<HTMLDivElement>(null);
 
-            <p className="small-about-text">
-              Na MOV acreditamos que cada projeto merece um toque único para ser
-              inesquecível. Estamos prontos para transformar suas ideias em
-              imagens que emocionam, engajam e inspiram. Conte com a gente para
-              dar vida à sua visão e mostrar o melhor da sua história. Vamos
-              criar algo incrível juntos!
-            </p>
-          </Box>
-          <Box className="small-about-icons">os ícones vão ficar aqui.</Box>
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          setIsVisible(true);
+          observer.disconnect(); // Executa a animação apenas uma vez
+        }
+      },
+      { threshold: 0.3 } // A visibilidade é detectada quando 30% da seção está visível
+    );
+
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current);
+    }
+
+    return () => {
+      if (sectionRef.current) observer.unobserve(sectionRef.current);
+    };
+  }, []);
+
+  return (
+    <MovLayout maxWidthContainer="100vw">
+      <Box
+        ref={sectionRef}
+        className={`small-about-container ${isVisible ? "visible" : ""}`}
+      >
+        <Box className="small-about-header">
+          <h1 className="small-about-title">Quem somos</h1>
+          <hr className="small-about-divider" />
         </Box>
-      </MovLayout>
-    </>
+        <Box className="small-about-description">
+          <h1 className="small-about-subtitle">Bem-vindo à MOV Produtora!</h1>
+          <p className="small-about-text">
+            Aqui transformamos suas ideias em imagens incríveis! Com mais de
+            1000 vídeos produzidos e centenas de clientes satisfeitos, somos
+            especialistas em capturar momentos inesquecíveis e criar conteúdos
+            visuais de alta qualidade. Nossos serviços incluem:
+          </p>
+          <ul className="custom-checklist">
+            <li className="small-about-text">
+              Fotografia Corporativa e Institucional: Capturamos a essência da
+              sua marca, destacando o que a torna autêntica e memorável.
+            </li>
+            <li className="small-about-text">
+              Cobertura de Eventos: De aniversários a eventos corporativos,
+              registramos cada detalhe com cuidado e criatividade para eternizar
+              esses momentos especiais.
+            </li>
+            <li className="small-about-text">
+              Filmagens Aéreas: Oferecemos uma visão diferenciada e
+              impressionante, realçando a beleza e a grandiosidade de cada
+              projeto.
+            </li>
+            <li className="small-about-text">
+              Animações e Motion Graphics: Transformamos ideias em animações
+              envolventes, que informam e encantam o público.
+            </li>
+          </ul>
+          <p className="small-about-text">
+            Na MOV acreditamos que cada projeto merece um toque único para ser
+            inesquecível. Estamos prontos para transformar suas ideias em
+            imagens que emocionam, engajam e inspiram. Conte com a gente para
+            dar vida à sua visão e mostrar o melhor da sua história. Vamos criar
+            algo incrível juntos!
+          </p>
+        </Box>
+        <Box className="small-about-icons">os ícones vão ficar aqui.</Box>
+      </Box>
+    </MovLayout>
   );
 };
